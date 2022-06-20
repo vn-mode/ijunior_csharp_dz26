8df0c4857dbd3362ec0a1d221062b355f740336c
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-06-20 19:00:51 +0300

    dz26
    dz26

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fishString = "Приветствую вас в моём демо коде.";
+            string[] tempArray = fishString.Split(' ');
+
+            foreach (var item in tempArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
